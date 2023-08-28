aef84eb29c8aff80c6b0dcc7b388aa444c94af9f
Isuru <isurusajith0@gmail.com>
2023-08-28 18:07:33 +0530

    error found,
    it is critical and so important , the case is ive give database name is "db-hibernate" but dash is not allowed for database name then i changed it to hibernate_self, now its working

--- src/main/java/lk/isuru/self/jpa/AppInitializer.java
+++ src/main/java/lk/isuru/self/jpa/AppInitializer.java
@@ -9,11 +9,11 @@ public class AppInitializer {
 
         try (SessionFactory sf = HibernateUtil.getSessionFactory();
              Session session = sf.openSession()) {
-            session.beginTransaction();
+            //session.beginTransaction();
 
             System.out.println(session);
 
-            session.getTransaction().commit();
+            //session.getTransaction().commit();
         }
     }
 }
