70d4ddb1174ab7fcbf703bbacba12503e15957f2
Isuru <isurusajith0@gmail.com>
2023-08-29 11:07:48 +0530

    bag entity created

---
+++ src/main/java/lk/isuru/self/jpa/entity/Bag.java
@@ -0,0 +1,23 @@
+package lk.isuru.self.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "bag")
+public class Bag {
+    @Id
+    private int id;
+    @Column(nullable = false)
+    private String brand;
+}
